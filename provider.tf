a476cf869d5e3b6d6b0b8e548b9d9473e9188b09
Jake Harding <jake.harding@icloud.com>
2026-02-25 22:00:44 +0000

    Move Terraform state to S3 backend with locking

--- provider.tf
+++ provider.tf
@@ -9,6 +9,14 @@ terraform {
       version = "~> 3.0"
     }
   }
+
+  backend "s3" {
+    bucket         = "jake-terraform-state-f48a66b9"
+    key            = "global/terraform.tfstate"
+    region         = "eu-west-2"
+    dynamodb_table = "terraform-locks"
+    encrypt        = true
+  }
 }
 
 provider "aws" {
