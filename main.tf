67d8043bd51f7c817b530ff54347b6ef4453a62e
Jake Harding <jake.harding@icloud.com>
2026-03-12 21:33:24 +0000

    Add VPC module with public and private subnets

--- main.tf
+++ main.tf
@@ -28,3 +28,31 @@ resource "aws_s3_bucket_versioning" "versioning" {
     status = "Enabled"
   }
 }
+
+module "vpc" {
+  source  = "terraform-aws-modules/vpc/aws"
+  version = "5.8.1"
+
+  name = "terraform-foundations-${var.environment}"
+  cidr = "10.0.0.0/16"
+
+  azs             = ["eu-west-2a", "eu-west-2b"]
+  public_subnets  = ["10.0.1.0/24", "10.0.2.0/24"]
+  private_subnets = ["10.0.101.0/24", "10.0.102.0/24"]
+
+  enable_nat_gateway = true
+  single_nat_gateway = true
+
+  enable_dns_hostnames = true
+  enable_dns_support   = true
+
+  tags = local.common_tags
+
+  public_subnet_tags = {
+    Name = "terraform-foundations-public"
+  }
+
+  private_subnet_tags = {
+    Name = "terraform-foundations-private"
+  }
+}
